f5d0c3e906c4d208eeb405c7ce9f883064851c7e
redcooltomato <redcooltomato@gmail.com>
2026-07-26 17:14:21 +0300

    verbose error handling

--- min-cp/main.c
+++ min-cp/main.c
@@ -6,11 +6,17 @@
 #include <string.h>
 
 
+void handle_open_errno(char *file_opened);
+
+void handle_rw_errno(char *file_opened);
+
 int main(int argc, char *argv[])
 {
     char src_path[PATH_MAX], dest_path[PATH_MAX];
 
     switch (argc) {
+        case 0:
+            return 1; // can't really happen
         case 1:
             printf("usage:\n%s [source] [destination]\n", argv[0]);
             return 0;
@@ -29,12 +35,13 @@ int main(int argc, char *argv[])
 
     int src = open(src_path, O_RDONLY);
     if (src == -1) {
-        printf("error occured while opening source file: %d\n", errno);
+        handle_open_errno("source");
         return 1;
     }
+
     int dest = open(dest_path, O_WRONLY);
     if (dest == -1) {
-        printf("error occured while opening dest file: %d\n", errno);
+        handle_open_errno("destination");
         return 1;
     }
 
@@ -47,18 +54,60 @@ int main(int argc, char *argv[])
         if (read_ret == -1) break;
 
         write_ret = write(dest, buf, read_ret);
-    } while (read_ret > 0 && read_ret == RW_SIZE && write_ret >= 0);
+    } while (read_ret == RW_SIZE && read_ret > 0 && write_ret >= 0);
 
     if (read_ret == -1) {
-        printf("error occured while reading: %d\n", errno);
+        handle_rw_errno("source");
         return 1;
     }
     if (write_ret == -1) {
-        printf("error occured while writing: %d\n", errno);
+        handle_rw_errno("destination");
         return 1;
     }
 
     printf("success!\n");
 
     return 0;
+}
+
+void handle_open_errno(char *file_opened)
+{
+    switch (errno) {
+        default:
+            printf("error occured while opening %s file: %d\n", file_opened, errno);
+            break;
+        case ENOENT:
+            printf("%s file doesn't exist\n", file_opened);
+            break;
+        case EACCES:
+            printf("not enough permissions to open %s file\n", file_opened);
+            break;
+        case ETXTBSY:
+            printf("%s is currently being executed\n", file_opened);
+            break;
+    }
+}
+
+void handle_rw_errno(char *file_opened)
+{
+    switch (errno) {
+        default:
+            printf("error occured while reading: %d\n", errno);
+            break;
+        case EINTR:
+            printf("%s was interrupted\n", file_opened);
+            break;
+        case EIO:
+            printf("io error on %s\n", file_opened);
+            break;
+        case EISDIR:
+            printf("%s file is a directory\n", file_opened);
+            break;
+        case ENOSPC:
+            printf("no space left to write to destination file\n"); // this one is obvious
+            break;
+        case EPERM:
+            printf("%s file is sealed\n", file_opened);
+            break;
+    }
 }
